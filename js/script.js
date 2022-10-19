a6f2428c9e4abddb3731781f8c9295c5347c993c
halleluiaz <halleluiazeyohannes@gmail.com>
2022-10-19 16:02:40 -0400

    more styling, inital script

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*
+File: js/script.js
+GUI Assignment: Creating an Interative Dynamic Table
+Halleluia Zeyohannes, UMass Lowell Computer Science, 
+halleluia_zeyohannes@student.uml.edu
+Copyright (c) 2022 by Halleluia Zeyohannes. All rights reserved. May be freely
+copied or excerpted for educational purposes with credit to the author.
+updated by HZ on October 19, 2022 at 3PM
+
+Purpose:
+*/
+
+function generateTable(multiplierMin, multiplierMax, multiplicandMin, multiplicandMax)
+{
+    var table = document.createElement('table');
+    table.id = 'table';
+
+    for(var row = multiplicandMin - 1; row <= multiplicandMax; row++)
+    {
+
+    }
+
+    return table;
+}
