98b4dd668862d3f7b82d9187f6078cc9afd7fff8
halleluiaz <halleluiazeyohannes@gmail.com>
2022-10-31 13:17:57 -0400

    form reqs

--- js/script.js
+++ js/script.js
@@ -14,11 +14,48 @@ function generateTable(multiplierMin, multiplierMax, multiplicandMin, multiplica
 {
     var table = document.createElement('table');
     table.id = 'table';
+    var headerX = true;  //  keeps track of table cells in first row
+    var headerY = true;  //  keeps track of table cells in first column
 
     for(var row = multiplicandMin - 1; row <= multiplicandMax; row++)
     {
-
+        var tableRow = document.createElement('tr');  // create new table row
+        for(var column = multiplierMin - 1; column <= multiplierMax; column++)
+        {
+            var cell;  // to contain the element of the cells in the table
+            var content;  // to contain the contents of each cell
+            if(headerX)
+            {
+                cell = document.createElement('th');
+                if(headerY == false)
+                {
+                    content = document.createTextNode(column);
+                    cell.appendChild(content);
+                }
+            }
+            else
+            {
+                if(headerY)
+                {
+                    cell = document.createElement('th');
+                    content = document.createTextNode(row);
+                    cell.appendChild(content);
+                }
+                else{
+                    cell = document.createElement('td');
+                    content = document.createTextNode(row*column);
+                    cell.appendChild(content);
+                }
+            }
+            tableRow.appendChild(cell);
+            headerY = false;
+        }
+        table.appendChild(tableRow);
+        headerX = false;
+        headerY = true;
     }
 
     return table
 }
+
+// need form validation
